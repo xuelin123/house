cc87ad4eebf243804f05344157233591e527867c
xuelin <1483403492@qq.com>
2019-08-25 21:36:28 +0800

    组员A添加的代码

--- src/main/java/com/team/house/controller/C.java
+++ src/main/java/com/team/house/controller/C.java
@@ -4,14 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class A {
+public class C {
 
     @RequestMapping("/go")
     public String gogo(){
         return "go";
     }
-    @RequestMapping("/go1")
-    public String gogo1(){
-        return "go";
-    }
+
 }
